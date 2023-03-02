e8a5aef3c18766f5076d56733e3c5374da9bb2e1
maheshwari <archit.maheshwari@payufin.com>
2023-03-02 13:29:43 +0530

    Dead cards function

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+
+	hand, remainingDeck := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("#########")
+	remainingDeck.print()
+
 }
