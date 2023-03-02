e4e1490ecfa2c038dd8e333020f53bb1badb85dc
maheshwari <archit.maheshwari@payufin.com>
2023-03-02 12:46:08 +0530

    newDeck method

--- main.go
+++ main.go
@@ -1,19 +1,6 @@
 package main
 
 func main() {
-	// var card string = "Ace of Spades"
-	// card := newCard()
-	// fmt.Println(card)
-
-	// Slice
-	cards := deck{"Archit", "Casper", newCard()}
-
-	// It doesn't modify the existing slice, returns a new slice
-	cards = append(cards, "Anupam")
-
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
