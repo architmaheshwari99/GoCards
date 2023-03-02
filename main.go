12fb1f3e5b1eb563e313e13e00254de0e4a46c14
maheshwari <archit.maheshwari@payufin.com>
2023-03-02 12:37:16 +0530

    using receiver in main.go

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := newCard()
 	// fmt.Println(card)
 
 	// Slice
-	cards := []string{"Archit", "Casper", newCard()}
+	cards := deck{"Archit", "Casper", newCard()}
 
 	// It doesn't modify the existing slice, returns a new slice
 	cards = append(cards, "Anupam")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
