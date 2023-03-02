5fe281776f16a739bb0c8e44c03cb37b010e3487
maheshwari <archit.maheshwari@payufin.com>
2023-03-02 18:16:44 +0530

    pointers in GO

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
+import "fmt"
+
+// func main() {
+// 	cards := newDeck()
+// 	cards.shuffle()
+// 	cards.print()
+// }
+
+type contact struct {
+	address string
+	phone   string
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contact
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	alex := person{firstName: "Archit", lastName: "Maheshwari", contact: contact{address: "Modinagar", phone: "243399"}}
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
+	alexPointer := &alex
+	alexPointer.updateName("Rajat")
+	alex.print()
+}
+
+func (pointerToPerson *person) updateName(name string) {
+	(*pointerToPerson).firstName = name
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
