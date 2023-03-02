974e7e53f76af3294e60bc943b668d10acf7a945
maheshwari <archit.maheshwari@payufin.com>
2023-03-02 13:42:37 +0530

    toString deck to write it in the file

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of dec
 // which is a slice of strings
@@ -32,4 +35,8 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	fmt.Println("#########")
-	remainingDeck.print()
+	fmt.Println(cards.toString())
 
 }
