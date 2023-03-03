21e88a05752a8fd3a770003f2fabfe206be1e96e
maheshwari <archit.maheshwari@payufin.com>
2023-03-03 23:13:28 +0530

    custom Write function

--- http/main.go
+++ http/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -17,6 +20,13 @@ func main() {
 	// // bs is itself updates as it is called by reference
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
 
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(b []byte) (int, error) {
+	fmt.Println(string(b))
+	return len(b), nil
 }
